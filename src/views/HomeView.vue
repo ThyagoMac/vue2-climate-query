42d4eb52b6b9ccd98b3b3358728b41fdd6a74059
thyagomac <thyago.cormick@gmail.com>
2024-08-10 10:35:53 -0300

    types and services;

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,21 @@
 <template>
-  <hello-world />
+  <v-container>
+    <div class="purple darken-2 text-center">
+      <span class="white--text">Lorem ipsum</span>
+      https://api.openweathermap.org/data/2.5/weather?lat=-15.868607497813757&lon=-47.82130864112691&lang=pt_br&appid=83bbcf10c1644a357a6a29b9c6d66fa0
+    </div>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "HomePage",
 
-  components: {
-    HelloWorld,
+  components: {},
+  created() {
+    console.log("created");
   },
 });
 </script>
